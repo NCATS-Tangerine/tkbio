1e18293181e566af656eff03b5a6c766131dfef7
lhannest <lhannest@gmail.com>
2017-04-26 10:20:21 -0700

    Fixed a null pointer bug in ConceptsApiController

--- api/server/src/main/java/bio/knowledge/server/api/ConceptsApiController.java
+++ api/server/src/main/java/bio/knowledge/server/api/ConceptsApiController.java
@@ -2,6 +2,7 @@ package bio.knowledge.server.api;
 
 import bio.knowledge.database.repository.ConceptRepository;
 import bio.knowledge.model.Concept;
+import bio.knowledge.model.neo4j.Neo4jConcept;
 import bio.knowledge.server.model.InlineResponse200;
 import bio.knowledge.server.model.InlineResponse2001;
 import bio.knowledge.server.model.InlineResponse2001DataPage;
@@ -69,12 +70,12 @@ public class ConceptsApiController implements ConceptsApi {
 		}
 
 		String[] filter = q.toArray(new String[q.size()]);
-		List<Concept> concepts;
-		if (sg != null) {
+		List<Neo4jConcept> concepts;
+		if (sg == null || sg.isEmpty()) {
+			concepts = conceptRepository.apiGetConcepts(filter, pageNumber, pageSize);
+		} else {
 			String[] semanticGroups = sg.toArray(new String[sg.size()]);
 			concepts = conceptRepository.apiGetConceptsByType(filter, semanticGroups, pageNumber, pageSize);
-		} else {
-			concepts = conceptRepository.apiGetConcepts(filter, pageNumber, pageSize);
 		}
 		
 		InlineResponse2001 response = new InlineResponse2001();
