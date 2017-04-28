a75f291532b4a95dbe33620c679d29b76873fcdc
Richard Bruskiewich <richard@starinformatics.com>
2017-04-28 11:27:23 -0700

    tweak?

--- api/server/src/main/java/bio/knowledge/server/api/ExactmatchesApi.java
+++ api/server/src/main/java/bio/knowledge/server/api/ExactmatchesApi.java
@@ -1,24 +1,24 @@
 package bio.knowledge.server.api;
 
-import java.util.List;
+import bio.knowledge.server.model.InlineResponse2002;
 
-import javax.validation.constraints.NotNull;
-
+import io.swagger.annotations.*;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 
-import bio.knowledge.server.model.InlineResponse2002;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import java.util.List;
+import javax.validation.constraints.*;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2017-04-27T09:43:12.446-07:00")
 
 @Api(value = "exactMatches", description = "the exactMatches API")
-public interface ExactmatchesApi {
+public interface ExactMatchesApi {
 
     @ApiOperation(value = "", notes = "Given an input list of [CURIE](https://www.w3.org/TR/curie/) identifiers of known exactly matched concepts [sensa SKOS](http://www.w3.org/2004/02/skos/core#exactMatch), retrieves the list of (CURIE)[https://www.w3.org/TR/curie/] identifiers of additional concepts that are deemed to be exact matches to one or more of the input concepts. This new list of concept identifiers is returned with the full list of any additional identifiers deemed by the KS to also be identifying exactly matched concepts. ", response = InlineResponse2002.class, responseContainer = "List", tags={ "exactmatches", })
     @ApiResponses(value = { 
--- api/server/src/main/java/bio/knowledge/server/api/ExactmatchesApiController.java
+++ api/server/src/main/java/bio/knowledge/server/api/ExactmatchesApiController.java
@@ -1,20 +1,26 @@
 package bio.knowledge.server.api;
 
-import java.util.List;
+import bio.knowledge.server.model.InlineResponse2002;
 
-import javax.validation.constraints.NotNull;
+import io.swagger.annotations.*;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 
-import bio.knowledge.server.model.InlineResponse2002;
-import io.swagger.annotations.ApiParam;
+import java.util.List;
+
+import javax.validation.constraints.*;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2017-04-27T09:43:12.446-07:00")
 
 @Controller
-public class ExactmatchesApiController implements ExactmatchesApi {
+public class ExactMatchesApiController implements ExactMatchesApi {
 
 
 
