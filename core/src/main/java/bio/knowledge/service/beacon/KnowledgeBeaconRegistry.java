ca22f66652bf0d8bdd5d1cf32ca12f3775fbe5b1
Kenneth Bruskiewicz <Kenneth@Kenneths-MacBook-Pro.local>
2017-05-04 16:07:37 -0700

    Separate knowledge sources specified in configuration file

--- core/src/main/java/bio/knowledge/service/beacon/KnowledgeBeaconRegistry.java
+++ core/src/main/java/bio/knowledge/service/beacon/KnowledgeBeaconRegistry.java
@@ -1,25 +1,60 @@
 package bio.knowledge.service.beacon;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
+import au.com.bytecode.opencsv.CSVReader;
 import bio.knowledge.client.ApiClient;
 
 @Service
+@PropertySource("classpath:application.properties")
 public class KnowledgeBeaconRegistry {
 	private List<ApiClient> apiClients = new ArrayList<ApiClient>();
 
+	@Value("${knowledgeBeacon.table.path}")
+	private String knowledgeBeaconTablePath;
+	
 	@PostConstruct
 	public void init() {
 		// TODO: Once we've got our own knowledge.bio beacon online, change the
 		// default URL. We always want our own knowledge source in the pool
 		// (though in the future we could give the user the opportunity to
 		// remove a knowledge source).
-		addKnowledgeSource("http://localhost:8090/api/");
+		assert (knowledgeBeaconTablePath != null);
+		System.out.println(knowledgeBeaconTablePath);
+
+		try {
+			CSVReader knowledgeSourceReader = new CSVReader(new FileReader(knowledgeBeaconTablePath));
+			List<String[]> knoweldgeSourceUrls = knowledgeSourceReader.readAll();
+			Iterator<String[]> ksuIterator = knoweldgeSourceUrls.iterator();
+			while (ksuIterator.hasNext()) {
+				String url = ksuIterator.next()[0];
+				addKnowledgeSource(url);
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			
+			e.printStackTrace();
+		}
+
+	}
+
+	private boolean check(String url) {		
+		// TODO: Check if URL is satisfying syntax here
+		return true;
 	}
 
 	/**
