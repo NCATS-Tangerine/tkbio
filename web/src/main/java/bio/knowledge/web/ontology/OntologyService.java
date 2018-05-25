8d66c7ea2f3e9fec3e4aac7dabaf60b5d1e31a22
Lance Hannestad <lhannest@sfu.ca>
2018-05-25 16:25:31 -0700

    Added OntologyService

---
+++ web/src/main/java/bio/knowledge/web/ontology/OntologyService.java
@@ -0,0 +1,48 @@
+package bio.knowledge.web.ontology;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Service;
+
+import bio.knowledge.ontology.BiolinkClass;
+import bio.knowledge.ontology.BiolinkModel;
+import bio.knowledge.ontology.BiolinkSlot;
+import bio.knowledge.ontology.mapping.InheritanceLookup;
+import bio.knowledge.ontology.mapping.ModelLookup;
+
+/**
+ * A "BiolinkClass" is what we call a category. A "BiolinkSlot" is what we call a predicate.
+ */
+@Service
+public class OntologyService {
+	
+	BiolinkModel model;
+	
+	InheritanceLookup<BiolinkClass> classInheritanceLookup;
+	ModelLookup<BiolinkClass> classModelLookup;
+	
+	InheritanceLookup<BiolinkSlot> slotInheritanceLookup;
+	ModelLookup<BiolinkSlot> slotModelLookup;
+	
+	@PostConstruct
+	public void init() {
+		model = BiolinkModel.get();
+		
+		classInheritanceLookup = new InheritanceLookup<>(model.getClasses());
+		classModelLookup = new ModelLookup<>(model.getClasses(), classInheritanceLookup);
+		
+		slotInheritanceLookup = new InheritanceLookup<>(model.getSlots());
+		slotModelLookup = new ModelLookup<>(model.getSlots(), slotInheritanceLookup);
+	}
+	
+	public BiolinkClass getClassByName(String biolinkClassName) {
+		return classModelLookup.getClassByName(biolinkClassName);
+	}
+	
+	public List<BiolinkClass> getClasses() {
+		return model.getClasses();
+	}
+
+}
